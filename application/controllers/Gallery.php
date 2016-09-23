d75ee3a7091385b8761d10e7c45dd5a5245c675d
Jason Kolenosky <JasonK58@gmail.com>
2016-09-23 12:11:24 -0700

    Altered controllers to render page content.

--- application/controllers/Gallery.php
+++ application/controllers/Gallery.php
@@ -10,7 +10,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Gallery extends application {
 	public function index()
 	{
-		$this->load->view('Gallery');   
+            $this->data['pagebody'] = 'gallery';
+            $this->render();  
 	}
-       
+
 }
